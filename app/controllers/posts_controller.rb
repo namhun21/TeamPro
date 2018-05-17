3c64c3f61a1c6cf493d5019a737e2bc69f56c298
EC2 Default User <ec2-user@ip-172-31-18-140.us-east-2.compute.internal>
2018-05-17 08:37:03 +0000

    댓글 수정 및 삭제 기능 추가

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_ownership, only: [:edit, :update, :destroy]
+  
   def index
     @posts = Post.all.order('created_at desc')
     @posts_count = current_user.posts.length
@@ -15,5 +17,31 @@ class PostsController < ApplicationController
     else
       redirect_to new_post_path
     end
+  end
+  
+  def edit
+  end
+  
+  def update
+    @post.content = params[:content]
+    
+    if @post.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+  
+    @post.destroy
+    redirect_to root_path
+  end
+  
+  private
+  
+  def check_ownership
+    @post = Post.find_by(id: params[:id])
+    redirect_to root_path if @post.user_id != current_user.id
   end
 end
