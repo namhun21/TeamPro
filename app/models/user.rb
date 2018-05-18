fa441cf249d989a2e3a68cb47a381b7a6755f669
EC2 Default User <ec2-user@ip-172-31-18-140.us-east-2.compute.internal>
2018-05-18 13:37:48 +0000

    글에 댓글쓰기 기능 추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :posts
+  has_many :likes
+  has_many :liked_posts, through: :likes, source: :post
+  has_many :comments
+  
+  def is_like?(post)
+    Like.find_by(user_id: self.id , post_id: post.id).present?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_131502) do
+ActiveRecord::Schema.define(version: 2018_05_18_125413) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
