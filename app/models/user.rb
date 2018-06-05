7a9fc724c09515a872e3a6fec2a2826a938c6b35
EC2 Default User <ec2-user@ip-172-31-20-208.us-east-2.compute.internal>
2018-06-05 09:49:07 +0000

    팔로우 버튼 안눌리는거 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :follower_relations, foreign_key: "followed_id", class_name: "Follow"
   has_many :followers, through: :follower_relations, source: :follower
-  has_many :following_relation, foreign_key: "follower_id", class_name: "Follow"
-  has_many :followings, through: :following_relation, source: :followed
+  has_many :following_relations, foreign_key: "follower_id", class_name: "Follow"
+  has_many :followings, through: :following_relations, source: :followed
   mount_uploader :avatar, AvatarUploader
   
   
