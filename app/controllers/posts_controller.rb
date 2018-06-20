61f07805defc95a7c34e8b090d11188e362bceb9
EC2 Default User <ec2-user@ip-172-31-14-207.us-east-2.compute.internal>
2018-06-20 14:39:37 +0000

    주석 추가

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show] #사용자가 로그인한 사용자만 접근 가능하도록 하기위해
   before_action :check_ownership, only: [:edit, :update, :destroy]
   
   def index
     @posts = Post.order('created_at desc').page params[:page]
-    @posts_count = current_user.posts.length
+    @posts_count = current_user.posts.length #현재 유저가 작성한 글의 수
   end
   
   def show
@@ -18,10 +18,10 @@ class PostsController < ApplicationController
   end
   
   def create
-    new_post = Post.new(user_id: current_user.id, content: params[:content], image: params[:image])
-    if new_post.save
-      redirect_to root_path
-    else
+    new_post = Post.new(user_id: current_user.id, content: params[:content], image: params[:image]) #로그인한 유저의 아이디와 내용은 파라미터로 온 것을 저장
+    if new_post.save #데이터베이스에 저장이 성공하면 루트로 이동
+      redirect_to root_path 
+    else  #실패하면 다시 작성하도록
       redirect_to new_post_path
     end
   end
