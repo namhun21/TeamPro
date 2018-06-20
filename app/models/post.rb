0b561b98ab520003ad80ce7ce8edd954fe0c8456
EC2 Default User <ec2-user@ip-172-31-14-207.us-east-2.compute.internal>
2018-06-20 14:24:00 +0000

    이미지 없으면 못돌리도록 및 기타 수정

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ApplicationRecord
     has_many :comments
     mount_uploader :image, PostImageUploader
     paginates_per 5  #페이지에 글이 5개만 나오도록 제한을 두었다.
+    validates :image, :presence=> { :message => "Please put the picture!"} #이미지를 업로드하지 않으면 글이 써지지 않도록
 end
