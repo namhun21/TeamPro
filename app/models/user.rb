0c018e6cd426d06419068bbe9194ddb24411a4c3
EC2 Default User <ec2-user@ip-172-31-14-207.us-east-2.compute.internal>
2018-06-20 15:30:44 +0000

    최종 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   has_many :posts
   has_many :likes
-  has_many :liked_posts, through: :likes, source: :post
+  has_many :liked_posts, through: :likes, source: :post #어떤 사용자에 대해 특정글을 좋아요 눌렀는지 확인하는 메소드
   has_many :comments
   has_many :follower_relations, foreign_key: "followed_id", class_name: "Follow"
   has_many :followers, through: :follower_relations, source: :follower
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   
   
-  def is_like?(post)
+  def is_like?(post) #특정post에 대한 정보를 넘김-> 좋아요 했는지 안했는지 확인
     Like.find_by(user_id: self.id , post_id: post.id).present?
   end
 end
